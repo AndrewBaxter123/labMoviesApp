44d02c608e5f776aebb1231f1cffe741b890d130
Andrew Baxter <74902103+AndrewBaxter123@users.noreply.github.com>
2022-10-21 00:47:20 +0100

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -5,32 +5,46 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+
 
 const MovieHeader = (props) => {
+  let faveMovie;
   const movie = props.movie;
+  const favouriteMovies = JSON.parse(localStorage.getItem("favourites")); 
+  favouriteMovies.forEach(item => {
+    (item.id === movie.id) ? (faveMovie = true) : (faveMovie = false);
+  })
 
   return (
     <Paper 
-        component="div" 
-        sx={{
-            display: "flex",
-            justifyContent: "space-around",
-            flexWrap: "wrap",
-            padding: 1.5,
-            margin: 0,
-        }}
-      >
+    component="div" 
+    sx={{
+        display: "flex",
+        justifyContent: "space-around",
+        flexWrap: "wrap",
+        padding: 1.5,
+        margin: 0,
+    }}
+  >
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
 
+      { 
+      faveMovie ? (
+        <Avatar sx={{ backgroundColor: 'red' }}>
+          <FavoriteIcon />
+        </Avatar>
+      ) : null
+      }
       <Typography variant="h4" component="h3">
         {movie.title}
         <a href={movie.homepage}>
           <HomeIcon color="primary" />
         </a>
-        <br />
-        <span sx={{ fontSize: "1.5rem" }}>{`   "${movie.tagline}"`} </span>
       </Typography>
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
