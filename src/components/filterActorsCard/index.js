169470b51e380c0d23ace70ff1923c1a6707d5bb
Andrew Baxter <74902103+AndrewBaxter123@users.noreply.github.com>
2022-11-08 14:05:56 +0000

    fixed Filtering for Actors by name.

--- src/components/filterActorsCard/index.js
+++ src/components/filterActorsCard/index.js
@@ -6,10 +6,15 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import SearchIcon from '@mui/icons-material/Search';
 import img from '../../images/pexels-dziana-hasanbekava-5480827.jpg'
+import { getActors } from "../../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from '../spinner'
 
 
 
 export default function FilterActorsCard(props) {
+    const { data, error, isLoading, isError } = useQuery("name", getActors);
+
 
   const handleChange = (e, type, value) => {
     e.preventDefault();
@@ -17,8 +22,16 @@ export default function FilterActorsCard(props) {
   };
 
   const handleTextChange = (e, props) => {
-    handleChange(e, "search", e.target.value);
+    handleChange(e, "name", e.target.value);
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
 
   return (
     <Card variant="outlined">
